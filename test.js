15667b66c7777871aae8b697d510995eccf050b0
mpbfx <1835886801@qq.com>
2026-01-15 23:20:02 +0800

    feat: 1/15

--- test.js
+++ test.js
@@ -1,21 +1,3 @@
-var reverseKGroup = (head, k) => {
-    let cur = head;
-    let count = 0;
-    while (count !== k && cur !== null) {
-        cur = cur.next;
-        count++;
-    }
-    if (count === k) {
-        let pre = null;
-        let node = head;
-        for (let i = 0; i < k; i++) {
-            let next = node.next;
-            node.next = pre;
-            pre = node;
-            node = next;
-        }
-        head.next = reverseKGroup(cur, k);
-        return pre;
-    }
-    return head;
+var genericSolution = function(head){
+    
 }
